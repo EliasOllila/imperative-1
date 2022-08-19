6e699fdcf5a3e2008e482263bf1b6537e00233ef
eliasollila <elias.ollila@syk.fi>
2022-08-19 11:16:30 +0300

    new stuff

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Input and output are fundamental computer operations would be, for example, finding the largest number and sorting a number group ");
+        System.out.println("2. A programming language has to have fixed vocabulary, unambiguous meaning and consistent grammar and syntax");
+        System.out.println("3. A compiler transforms the code into executable machine intruction file");
+        System.out.println("4. Constants are used when a value does not want to be changed.");
+    }
+}
